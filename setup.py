9625e34f205629d628026e7708cf90f6a2fb6b52
Duncan <duncanstothers3699@gmail.com>
2024-02-08 20:23:25 -0800

    pyval initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pyval',
+    version='0.1',
+    packages=find_packages(),
+    license='N/A',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Duncan Stothers',
+    author_email='duncanstothers3699.com',
+    url='https://github.com/Radar3699/pyval',
+)
